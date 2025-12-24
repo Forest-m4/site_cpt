47b66c44cea87e507535c7c7ce04bf79ed62e275
Forest-m4 <makarova.dariya@icloud.com>
2025-12-24 20:04:58 +0300

    feature-5

--- backend/src/lib/db.ts
+++ backend/src/lib/db.ts
@@ -1,10 +1,8 @@
 import { Pool } from 'pg';
 import { drizzle, PostgresJsDatabase } from 'drizzle-orm/postgres-js';
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-export const db = drizzle(pool) as PostgresJsDatabase;
+export const db: PostgresJsDatabase = drizzle(pool);
