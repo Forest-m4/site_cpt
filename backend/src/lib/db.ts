5f3be16f4340a541302b862312ca4e8feaa73020
Forest-m4 <makarova.dariya@icloud.com>
2025-12-25 00:27:03 +0300

    add drizzle and posts schema

--- backend/src/lib/db.ts
+++
@@ -1,8 +0,0 @@
-import { Pool } from 'pg';
-import { drizzle, PostgresJsDatabase } from 'drizzle-orm/postgres-js';
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
-
-export const db: PostgresJsDatabase = drizzle(pool);
