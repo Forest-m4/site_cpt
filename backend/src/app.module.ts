91e1c57712c8f8e445684967c61659500a080f86
Forest-m4 <makarova.dariya@icloud.com>
2025-12-26 19:15:57 +0300

    turbo-repo

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,36 @@
+import { DrizzlePGModule } from '@knaadh/nestjs-drizzle-pg';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import * as schema from '../src/lib/infrastructure/db/schema';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { APP_PIPE } from '@nestjs/core';
+import { ZodValidationPipe } from 'nestjs-zod';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DrizzlePGModule.registerAsync({
+      tag: 'DB_DEV',
+      inject: [ConfigService],
+      imports: [ConfigModule],
+      useFactory(config: ConfigService) {
+        return {
+          pg: {
+            connection: 'pool',
+            config: {
+              connectionString: config.getOrThrow<string>('DATABASE_URL'),
+            },
+          },
+          config: { schema: { ...schema } },
+        };
+      },
+    }),
+  ],
+  controllers: [],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useClass: ZodValidationPipe,
+    },
+  ],
 })
 export class AppModule {}
