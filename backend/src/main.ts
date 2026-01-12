29ad5d0a779eb1d5c425dbd6742ae3ec0749e0cf
Forest-m4 <makarova.dariya@icloud.com>
2026-01-12 19:51:11 +0300

    swagger+postman

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -13,5 +13,4 @@ async function bootstrap() {
   await app.listen(process.env.PORT ?? 3000);
 }
 
-// eslint-disable-next-line @typescript-eslint/no-floating-promises
-bootstrap();
+void bootstrap();
